f97343ff4596d2d4a26273b5b01bebdacb72de4f
penka-test1 <pepe_lina@mail.bg>
2026-09-21 12:49:24 +0300

    Refactor towns.js to eliminate duplicates
    Removed duplicate function definitions and cleaned up event bindings.

--- towns.js
+++ towns.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('#btnAdd').click(addTown);
     $('#btnShuffle').click(shuffleTowns);
     $('#btnAdd').click(addTown);
+    $('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -52,4 +53,22 @@ function shuffleTowns() {
     }
 }
 
-function addTown() { let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName)); $('#result').text(townName + " added."); }
+function addTown() { 
+    let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName)); 
+    $('#result').text(townName + " added."); 
+}
+
+function shuffleTowns() { 
+    let towns = $('#towns option').toArray(); 
+    $('#towns').empty();
+    shuffleArray(towns); $('#towns').append(towns); 
+    $('#result').text("Towns shuffled."); 
+    function shuffleArray(array) { 
+        for (var i = array.length - 1; i > 0; i--) { 
+            var j = Math.floor(Math.random() * (i + 1)); 
+            var oldElement = array[i]; 
+            array[i] = array[j]; array[j] = oldElement; 
+        } 
+    } 
+}
+                         
