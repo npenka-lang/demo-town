cf6d368e463b221d80b932b11222bae1951af267
Penka1 <pepe_lina@mail.bg>
2026-09-21 12:21:41 +0300

    Ïmplement finctionality to add new town

--- towns.js
+++ towns.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('#btnDelete').click(deleteTown);
     $('#btnAdd').click(addTown);
     $('#btnShuffle').click(shuffleTowns);
+    $('#btnAdd').click(addTown);
 });
 
 function deleteTown() {
@@ -50,3 +51,5 @@ function shuffleTowns() {
         }
     }
 }
+
+function addTown() { let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName)); $('#result').text(townName + " added."); }
